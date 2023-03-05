1ecdc426aeed7f470926450eb64b940f127ec327
ERos1012 <65523315+ERos1012@users.noreply.github.com>
2023-03-05 11:56:34 -0500

    First draft

--- src/card-list.js
+++ src/card-list.js
@@ -1,6 +1,7 @@
 import { LitElement, html, css } from 'lit';
-
-const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
+import "@lrnwebcomponents/meme-maker/meme-maker.js";
+import "erosfinal-character-card/src/character-card.js";
+import "erosfinal-character-card/src/card-meme-maker.js";
 
 class CardList extends LitElement {
   static properties = {
@@ -9,78 +10,24 @@ class CardList extends LitElement {
 
   static styles = css`
     :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--card-list-background-color);
-    }
-
-    main {
-      flex-grow: 1;
-    }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
+      display: inline-block;
     }
   `;
 
   constructor() {
     super();
-    this.header = 'My app';
+    this.header = 'My Card List';
   }
 
   render() {
-    return html`
-      <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
-
-        <p>Edit <code>src/CardList.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
+    return html` 
+    <p> ${this.header} </p>  
+     <erosfinal-character-card
+     characterName="Pikachu"
+     characterBio="Pikachu is a good boi"
+     accentColor="red"
+     meme="https://images.theconversation.com/files/38926/original/5cwx89t4-1389586191.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=926&fit=clip">
+     </erosfinal-character-card>
     `;
   }
 }
