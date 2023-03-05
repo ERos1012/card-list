a103b80a4780251a02d51f20e547ba8ab7a3f622
ERos1012 <65523315+ERos1012@users.noreply.github.com>
2023-03-05 12:50:22 -0500

    testing

--- src/card-list.js
+++ src/card-list.js
@@ -1,35 +1,44 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html, css } from "lit";
 import "@lrnwebcomponents/meme-maker/meme-maker.js";
 import "erosfinal-character-card/src/character-card.js";
-import "erosfinal-character-card/src/card-meme-maker.js";
 
 class CardList extends LitElement {
   static properties = {
     header: { type: String },
-  }
+  };
 
   static styles = css`
     :host {
-      display: inline-block;
+      display: block;
+      padding: 25px;
+      color: var(--card-list-text-color, #000);
     }
   `;
 
   constructor() {
     super();
-    this.header = 'My Card List';
+    this.header = "My Card List";
   }
 
   render() {
-    return html` 
-    <p> ${this.header} </p>  
-     <erosfinal-character-card
-     characterName="Pikachu"
-     characterBio="Pikachu is a good boi"
-     accentColor="red"
-     meme="https://images.theconversation.com/files/38926/original/5cwx89t4-1389586191.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=926&fit=clip">
-     </erosfinal-character-card>
+    return html`
+      <div class="main">
+        <p>${this.header}</p>
+        <meme-maker
+          top-text="Bulbasaur!"
+          bottom-text="I choose you!"
+          image="https://images.theconversation.com/files/38926/original/5cwx89t4-1389586191.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=926&fit=clip"
+        >
+        </meme-maker>
+        <erosfinal-character-card
+          characterName="Pikachu"
+          characterBio="Pikachu is a good boi"
+          characterImage="https://images.theconversation.com/files/38926/original/5cwx89t4-1389586191.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=926&fit=clip"
+        >
+        </erosfinal-character-card>
+      </div>
     `;
   }
 }
 
-customElements.define('card-list', CardList);
+customElements.define("card-list", CardList);
