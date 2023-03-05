eab4501806e160edb52acbd718a539f30a05e73d
ERos1012 <65523315+ERos1012@users.noreply.github.com>
2023-03-05 14:23:21 -0500

    work please

--- src/card-list.js
+++ src/card-list.js
@@ -1,6 +1,6 @@
 import { LitElement, html, css } from "lit";
 import "@lrnwebcomponents/meme-maker/meme-maker.js";
-import "erosfinal-character-card/src/character-card.js";
+import "erosfinal-character-card/src/professor-card2.js";
 
 class CardList extends LitElement {
   static properties = {
@@ -26,19 +26,8 @@ class CardList extends LitElement {
 
   render() {
     return html`
-        <p>${this.header}</p>
-        <meme-maker
-          top-text="Bulbasaur!"
-          bottom-text="I choose you!"
-          image="https://images.theconversation.com/files/38926/original/5cwx89t4-1389586191.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=926&fit=clip"
-        >
-        </meme-maker>
-        <erosfinal-character-card 
-          class="card"
-          characterName="Pikachu"
-          characterBio="Pikachu is a good boi"
-          characterImage="https://images.theconversation.com/files/38926/original/5cwx89t4-1389586191.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=926&fit=clip"
-        >
+        <erosfinal-character-card
+        subtitle="Eric">
         </erosfinal-character-card>
     `;
   }
